46f1667c612f5244fed236e89e2f1f8027835bc7
Bimsara Wijewantha <wijewanthabimsara@gmail.com>
2026-01-02 16:19:27 +0530

    refactor: rename application.properties to application.yml and configure OAuth2 clients
    -go the google 0auth page(access denied)

---
+++ src/main/java/com/google0auth/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.google0auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @GetMapping("public")
+    public String method1() {
+         return "this is the public method1";
+    }
+    @GetMapping("private")
+    public String method2() {
+            return "this is the public method2";
+    }
+}
