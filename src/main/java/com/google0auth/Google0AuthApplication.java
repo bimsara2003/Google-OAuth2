ef063c353b4edcacec97c6857635a06d80212312
Bimsara Wijewantha <wijewanthabimsara@gmail.com>
2026-01-02 15:02:50 +0530

    first commit

---
+++ src/main/java/com/google0auth/Google0AuthApplication.java
@@ -0,0 +1,13 @@
+package com.google0auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Google0AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Google0AuthApplication.class, args);
+    }
+
+}
